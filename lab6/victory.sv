978b81c1ab44d1455e7eafee813e4486f6e82fce
jas-soh <67356226+jas-soh@users.noreply.github.com>
2021-05-13 21:38:14 -0700

    Update victory.sv

--- lab6/victory.sv
+++ lab6/victory.sv
@@ -3,13 +3,13 @@ module victory(clk, rst, L, R, L_edge, R_edge, hex);
   input logic L_edge, R_edge;
   output logic [6:0] hex;
   
-  enum logic[6:0] { L_win=7'b1111110, R_win=7'b0111111, no_win=7'b1011111 } ps, ns;
+  enum logic[6:0] { L_win=7'b1001111, R_win=7'b0100100, no_win=7'b1111111 } ps, ns;
   
   always_comb begin
 	ns = no_win;
     case (ps)
-      no_win: if (L_edge & L & !R) ns = R_win;
-              else if (R_edge & R & !L) ns = L_win;
+      no_win: if (L_edge & L & !R) ns = L_win;
+              else if (R_edge & R & !L) ns = R_win;
       R_win: ns = R_win;
       L_win: ns = L_win;
 	endcase 
