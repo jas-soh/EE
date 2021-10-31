1cf870767dc96c5289761a49eea0638067186cf9
jas-soh <67356226+jas-soh@users.noreply.github.com>
2021-10-31 01:55:45 -0700

    Update task1b.h

--- EE474/lab2/task1b/task1b.h
+++ EE474/lab2/task1b/task1b.h
@@ -1,3 +1,45 @@
+/*
+#ifndef __task2_H__
+#define __task2_H__
+
+#define RCGCGPIO (*((volatile uint32_t *)0x400FE608))
+
+// Port L Address
+#define GPIODIR_L (*((volatile uint32_t *)0x40062400))
+#define GPIODEN_L (*((volatile uint32_t *)0x4006251C))
+#define GPIODATA_L (*((volatile uint32_t *)0x400623FC))
+
+#define GPIOAMSEL_L (*((volatile uint32_t *)0x40062528))
+#define GPIOAFSEL_L (*((volatile uint32_t *)0x40062420))
+
+
+// Port L Control
+#define PL0 0x1  // connect to System ON/OFF button
+#define PL1 0x2  // connect to Pedestrian ON/OFF button
+#define PL2 0x4  // connect to Red LED
+#define PL3 0x8  // connect to Yellow LED
+#define PL4 0x10 // connect to Green LED
+
+#define RCGCTIMER (*((volatile uint32_t *)0x400FE604))
+
+#define GPTMCTL_0 (*((volatile uint32_t *)0x4003000C))
+#define GPTMCFG_0 (*((volatile uint32_t *)0x40030000))
+#define GPTMTAMR_0 (*((volatile uint32_t *)0x40030004))
+#define GPTMTAILR_0 (*((volatile uint32_t *)0x40030028))
+#define GPTMRIS_0 (*((volatile uint32_t *)0x4003001C))
+#define GPTMICR_0 (*((volatile uint32_t *)0x40030024))
+
+#define GPTMCTL_1 (*((volatile uint32_t *)0x4003100C))
+#define GPTMCFG_1 (*((volatile uint32_t *)0x40031000))
+#define GPTMTAMR_1 (*((volatile uint32_t *)0x40031004))
+#define GPTMTAILR_1 (*((volatile uint32_t *)0x40031028))
+#define GPTMRIS_1 (*((volatile uint32_t *)0x4003101C))
+#define GPTMICR_1 (*((volatile uint32_t *)0x40031024))
+
+
+#endif //__HEADER1_H__
+*/
+
 #ifndef __HEADER1_H__
 #define __HEADER1_H__
 
@@ -8,35 +50,34 @@
 // Timer
 #define RCGCTIMER (*((volatile uint32_t *)0x400FE604))
 
-#define GPTMCFG0 (*((volatile uint32_t *)0x40030000))
-#define GPTMCFG1 (*((volatile uint32_t *)0x40031000))
+#define GPTMCFG_0 (*((volatile uint32_t *)0x40030000))
+#define GPTMCFG_1 (*((volatile uint32_t *)0x40031000))
 
-#define GPTMCTL0 (*((volatile uint32_t *)0x4003000C))
-#define GPTMCTL1 (*((volatile uint32_t *)0x4003100C))
+#define GPTMCTL_0 (*((volatile uint32_t *)0x4003000C))
+#define GPTMCTL_1 (*((volatile uint32_t *)0x4003100C))
 
-#define GPTMTAMR0 (*((volatile uint32_t *)0x40030004))
-#define GPTMTAMR1 (*((volatile uint32_t *)0x40031004))
+#define GPTMTAMR_0 (*((volatile uint32_t *)0x40030004))
+#define GPTMTAMR_1 (*((volatile uint32_t *)0x40031004))
 
-#define GPTMTAILR0 (*((volatile uint32_t *)0x40030028))
-#define GPTMTAILR1 (*((volatile uint32_t *)0x40031028))
+#define GPTMTAILR_0 (*((volatile uint32_t *)0x40030028))
+#define GPTMTAILR_1 (*((volatile uint32_t *)0x40031028))
 
-#define GPTMRIS0 (*((volatile uint32_t *)0x4003001C))
-#define GPTMRIS1 (*((volatile uint32_t *)0x4003001C))
+#define GPTMRIS_0 (*((volatile uint32_t *)0x4003001C))
+#define GPTMRIS_1 (*((volatile uint32_t *)0x4003101C))
 
-#define GPTMICR0 (*((volatile uint32_t *)0x40030024))
-#define GPTMICR1 (*((volatile uint32_t *)0x40031024))
+#define GPTMICR_0 (*((volatile uint32_t *)0x40030024))
+#define GPTMICR_1 (*((volatile uint32_t *)0x40031024))
 
 // Timer Configuation
-#define GPTM0_16_32 0x1           // Enable 16/32 Timer 0
-#define GPTM1_16_32 0x2           // Enable 16/32 Timer 1
+#define TIMER0_EN 0x1 // Enable 16/32 Timer 0
+#define TIMER1_EN 0x2 // Enable 16/32 Timer 1
 
-#define GPTMA_DISABLE 0x0         // Disable Timer A
-#define GPTMA_ENABLE 0x1          // Enable Timer A
-#define TM_MODE_32 0x0            // Select 32-bit mode
-#define TAMR_PER_TM_MODE 0x2      // Configure TAMR bit of GPTMTAMR to be in periodic timer mode
-#define TACDIR_COUNT_UP 0x10000   // Configure TACDIR bit of GPTMTAMR to count down
-#define TACDIR_COUNT_DOWN 0x10000 // Configure TACDIR bit of GPTMTAMR to count down
-#define N16_MIL 0xF42400          // load the value 16,000,000
+#define MODE_32_bit 0x0 // 32-bit mode
+#define PERIODIC_MODE 0x2 // Periodic timer mode
+#define COUNT_DOWN 0x10000 // TACDIR bit to count down
+#define VAL_16_MIL 0xF42400 // The value 16,000,000 in hex
+#define VAL_80_MIL 0x4C4B400 // The value 16,000,000 in hex
+
 
 // Port L Address
 #define GPIODIR_L   (*((volatile uint32_t *)0x40062400))
@@ -56,25 +97,26 @@
 void LED_init();
 
 // Turn on green light
-void Go_State();
+void go_output();
 
 // Turn on yellow light
-void Warn_State();
+void warn_output();
 
 // Turn on red light
-void Stop_State();
+void stop_output();
 
 // Turn off all LEDs
-void LED_off();
+void off_output();
 
 // reset the timer
 void resetTimer();
 
-// initialize the Timer0
-void GPTMInit();
+// initialize the Timer0 and Timer1
+void timer0_init();
+void timer1_init();
 
 // input variable n to keep n seconds
-void oneHertz(int n);
+int twoSeconds();
 
 // initialize the system switch and pedestrian switch, no input variable
 void extern_switch_init();
