40adf43b8bfa72d8f1c25e7621e008350e8e91ef
jas-soh <67356226+jas-soh@users.noreply.github.com>
2021-11-03 10:37:17 -0700

    Update task1b.h

--- EE474/lab2/task1b/task1b.h
+++ EE474/lab2/task1b/task1b.h
@@ -29,13 +29,11 @@
 // Timer Configuation
 #define GPTM0_16_32 0x1           // Enable 16/32 Timer 0
 
-#define GPTMA_DISABLE 0x0         // Disable Timer A
-#define GPTMA_ENABLE 0x1          // Enable Timer A
-#define TM_MODE_32 0x0            // Select 32-bit mode
-#define TAMR_PER_TM_MODE 0x2      // Configure TAMR bit of GPTMTAMR to be in periodic timer mode
+#define MODE_32 0x0            // Select 32-bit mode
+#define PERIODIC_MODE 0x2      // Configure TAMR bit of GPTMTAMR to be in periodic timer mode
 #define TACDIR_COUNT_UP 0x10000   // Configure TACDIR bit of GPTMTAMR to count down
-#define TACDIR_COUNT_DOWN 0x10000 // Configure TACDIR bit of GPTMTAMR to count down
-#define N16_MIL 0xF42400          // load the value 16,000,000
+#define COUNT_DOWN 0x10000 // Configure TACDIR bit of GPTMTAMR to count down
+#define VAL_16_MIL 0xF42400          // load the value 16,000,000
 
 // Port L Address
 #define GPIODIR_L   (*((volatile uint32_t *)0x40062400))
@@ -44,28 +42,8 @@
 #define GPIOAMSEL_L (*((volatile uint32_t *)0x40062528))
 #define GPIOAFSEL_L (*((volatile uint32_t *)0x40062420))
 
-// Port L Control
-#define PL0 0x1  // connect to System ON/OFF button
-#define PL1 0x2  // connect to Pedestrian ON/OFF button
-#define PL2 0x4  // connect to Red LED
-#define PL3 0x8  // connect to Yellow LED
-#define PL4 0x10 // connect to Green LED
-
 // initialize the RGB LEDs, no input variable
 void LED_init();
-
-// Turn on green light
-void Go_Output();
-
-// Turn on yellow light
-void Warn_Output();
-
-// Turn on red light
-void Stop_Output();
-
-// Turn off all LEDs
-void Off_Output();
-
 
 // initialize the Timer0
 void timer0_init();
