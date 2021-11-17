4216fbc1dfdf8da67b42de34f7cb0064142c39f6
jas-soh <67356226+jas-soh@users.noreply.github.com>
2021-11-16 19:11:44 -0800

    Update Lab3_Inits.c

--- EE474/lab3/task2a/Lab3_Inits.c
+++ EE474/lab3/task2a/Lab3_Inits.c
@@ -4,7 +4,7 @@
 
 #include "PLL_Header.h"
 #include "Lab3_Inits.h"
-#include "task1a.h"
+#include "task2a.h"
 
 // STEP 0a: Include your header file here
 // YOUR CUSTOM HEADER FILE HERE
@@ -56,47 +56,28 @@ int PLL_Init(enum frequency freq) {
     return 1;
 }
 
-void LED_Init(void) {
-  // STEP 1: Initialize the 4 on board LEDs by initializing the corresponding
-  // GPIO pins.
-
-  volatile unsigned short delay = 0 ;
-  RCGCGPIO |= PORT_F;
-  RCGCGPIO |= PORT_N;
-  delay++;
-  delay++;
-  
-  // enable digital functions
-  GPIODEN_F |= (LED3 | LED4);
-  GPIODEN_N |= (LED1 | LED2); 
-  
-  // to output
-  GPIODIR_F |= (LED3 | LED4);
-  GPIODIR_N |= (LED1 | LED2); 
-  
-  // initialize data
-  GPIODATA_F = 0;
-  GPIODATA_N = 0x0;
-}
-
 void Switches_Init(void) {
    volatile unsigned short delay = 0;
    RCGCGPIO |= PORT_J; // Enable PortJ(Switches)
    delay++; // Delay 2 more cycles before access Timer registers
    delay++; // Refer to Page. 756 of Datasheet for info
    
-   GPIODATA_N = 0x0;
+   //GPIODATA_N = 0x0;
    
    // Set up Port J for switches
    GPIODIR_J = 0x0; // Set PJ0 and PJ1 to input
-   GPIODEN_J = (SW2| SW1); // Set PJ0 and PJ1 to digital
+   GPIODEN_J = (SW2 | SW1); // Set PJ0 and PJ1 to digital
    GPIOPUR_J = (SW2 | SW1); // Set up pullup resistors
-   GPIOLOCK_J = 0x4C4F434B; // Unlock Port J
+   //GPIOLOCK_J = 0x4C4F434B; // Unlock Port J
 
    // Set up interrupts for switches
-   GPIOICR_J |= (SW2| SW1); // clear prior interrupt
-   GPIOIM_J |= (SW2| SW1); // unmask interrupts
-   EN1 |= ENABLE_51; // Enables Port J interrupt. Set field 51 to 1
+   EN1 = ENABLE_51; // Enables Port J interrupt. Set field 51 to 1
+   GPIOIM_J = 0x0; // Mask all interrupts
+   GPIOIS_J &= ~0x3; // Make bits 1 and 0 edge sensitive
+   GPIOIBE_J &= ~0x3; // Deactivate both edge detection
+   GPIOIEV_J &= ~0x3; // Falling edge
+   GPIOICR_J |= 0x3; // Clear Port J interruption
+   GPIOIM_J |= 0x3; // Unmask bits 1 and 0
 };
 
 
@@ -104,74 +85,96 @@ void ADCReadPot_Init(void) {
   volatile unsigned short delay = 0 ;
   // STEP 2: Initialize ADC0 SS3.
   // 2.1: Enable the ADC0 clock
-  RCGCADC |= 0x1;//ADC_0_EN;
-
+  RCGCADC |= (1<<0);
   // 2.2: Delay for RCGCADC (Refer to page 1073)
-  // must delay 3
   delay++;
   delay++;
   delay++;
-
   // 2.3: Power up the PLL (if not already)
   PLLFREQ0 |= 0x00800000; // we did this for you
   // 2.4: Wait for the PLL to lock
   while (PLLSTAT != 0x1); // we did this for you
   // 2.5: Configure ADCCC to use the clock source defined by ALTCLKCFG
-  //RCGCADC |= ADC_0_EN;
-  ADCCC_0 = 0x1;
-
-  // 2.6: Enable clock to the appropriate GPIO Modules (Hint: Table 15-1)
-  RCGCGPIO |= PORT_E;
-  GPIODIR_E |= 0x0; // input
+  ADCCC_0 = (0x1); 
 
   // 2.7: Delay for RCGCGPIO
   delay++;
   delay++;
-
-  // 2.8: Set the GPIOAFSEL bits for the ADC input pins
-  GPIOAFSEL_E |= 0x8; // to be controlled
-
-  // 2.9: Clear the GPIODEN bits for the ADC input pins
-  GPIODEN_E &= ~0x8; // disable digital
-
-  // 2.10: Disable the analog isolation circuit for ADC input pins (GPIOAMSEL)
-  GPIOAMSEL_E |= 0x8;
+  delay++;
 
   // 2.11: Disable sample sequencer 3 (SS3)
-  ADCACTSS_0 &= ~0x8; //checked
-
+  ADCACTSS_0 &= ~(1<<3); //ss3 disable
+  
   // 2.12: Select timer as the trigger for SS3
-  ADCEMUX_0 |= 0x5000; //checked
-
+  ADCEMUX_0 = (0x5<<12);
   // 2.13: Select the analog input channel for SS3 (Hint: Table 15-1)
-  ADCSSEMUX3_0 = 0x0; // checked
-  ADCSSMUX3_0 = 0xC; // Select AIN3
-
+  ADCSSMUX3_0 = 0x2; 
   // 2.14: Configure ADCSSCTL3 register
-  ADCSSCTL3_0 = 0x4 | 0x8;
-
+  ADCSSCTL3_0 = 0x6; 
   // 2.15: Set the SS3 interrupt mask
-  ADCIM_0 = 0x8; // 0x8 checked
-
+  ADCIM_0 |= (1<<3);
   // 2.16: Set the corresponding bit for ADC0 SS3 in NVIC
-  NVIC_EN0 |= (1 << 17);
+  ADCISC_0 |= (1<<3);
+  EN0 |= (0x1<<17);
+  // 2.17 setting up ADCSSOP3 for temperature read
+  ADCSSCTL3_0 |= (1<<3); 
 
   // 2.17: Enable ADC0 SS3
-  ADCACTSS_0 |= 0x8;
-
+  ADCACTSS_0 |= (1<<3);
 }
-
+    
 void TimerADCTriger_Init(void) {
-  // STEP 3: Initialize Timer0A to trigger ADC0 at 1 HZ.
-  // Hint: Refer to section 13.3.7 of the datasheet
-
-  RCGCTIMER |= 0x1; // Enable Timer 0
-  GPTMCTL_0 &= 0x0; // Disable 
-  GPTMCFG_0 = 0x0; // Set 32-bit configuration
-  GPTMTAMR_0 |= 0x2; // Set Periodic timer mode
-  GPTMTAMR_0 &= ~(0x10); // Set to count down mode
-  GPTMTAILR_0 = 0xF42400; // Load 16 million on to timer (1Hz)
-  GPTMADCEV_0 |= 0x1; // Enable ADC Event
-  GPTMCC_0 = 0x1;
-  GPTMCTL_0 |= 0x21; // Enable ADC trigger & timer0A
+  RCGCTIMER |= 0x1; // Enable timer 0 clock
+  GPTMCTL_0 = 0x0; // Disable timer 0
+  GPTMCTL_0 |= 0x20; // enable ADC output trigger
+  GPTMCFG_0 = 0x0; // Set 32-bit mode
+  GPTMTAMR_0 |= 0x2; // Set timer to periodic and countdown
+  GPTMTAILR_0 = 0xF42400; // 16M in hex
+  GPTMADCEV_0 |= 0x1; // enable trigger on timeout event
+  GPTMCC_0 = 0x1; // set the clock source to alternate clock
+  GPTMCTL_0 |= 0x1; // Enable timer 0
 }
+   
+
+void UART_Init(uint32_t bdi, uint32_t bdf) {
+//void UART_Init() {
+  volatile unsigned short delay = 0;
+  RCGCUART |= 0x1;//(PIN_0 | PIN_1);
+  RCGCGPIO |= PORT_A; // enable port A
+  
+  delay++;
+  delay++;
+  delay++; // delay for three clock cycles before further configuration
+  
+  GPIOAFSEL_A = (PIN_0 | PIN_1);
+  GPIODEN_A = (PIN_0 | PIN_1); 
+  GPIODR2R_A = (PIN_0 | PIN_1); // 2-mA drive to pin 0
+  GPIOPCTL_A = 0x11; // configuration for pin 0 and 1 (TX and RX)
+  
+  // disable UART by clearing UARTEN bit in UARTCTL reg
+  UARTCTL_0 &= ~0x1;       // Disable UART
+  //UARTCTL_0 |= (0x3 << 8); // Enable UART transmission/reception
+  
+  // BRD = 120,000,000/(16*9600) for freq 1
+  // BRD = 60,000,000/(16*9600) for freq 2
+  // BRD = 12,000,000/(16*9600) for freq 3
+  
+  // write integer portion of BRD in UARTIBRD reg
+  UARTIBRD_0 = bdi; // integer in hex 
+  //UARTIBRD_0 = 781; // integer in hex 
+  //UARTIBRD_0 = 0x68; // integer in hex 
+  
+  // write fraction portion of BRD to UARTFBRD reg
+  UARTFBRD_0 = bdf; // fraction in hex
+  //UARTFBRD_0 = 0xB; // fraction in hex
+  
+  // write desired serial parameter to UARTLCRH reg
+  UARTLCRH_0 |= (0x3 << 5); // no parity and one bit stop
+  
+  // write to UARTCC reg
+  UARTCC_0 = 0x0;// alternate clock source
+    
+  // enable UART by setting UARTEN bit in UARTCTL reg
+  UARTCTL_0 |= 0x1;
+}
+
